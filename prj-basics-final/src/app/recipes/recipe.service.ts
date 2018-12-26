ca772962b2661e65cbefc270998cffbed8986861
cmorredd <chaitu.riims@gmail.com>
2018-12-25 23:21:52 -0500

    New Recipe Section completed
    New Recipe Section completed- Edit Recipe and Delete Recipe functionality also added

--- prj-basics-final/src/app/recipes/recipe.service.ts
+++ prj-basics-final/src/app/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService{
+  recipesChanged=new Subject<Recipe[]>();
 	
  private recipes: Recipe[] = [
     new Recipe('A Test Recipe1', 'This is simply a test1', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
@@ -31,4 +33,19 @@ export class RecipeService{
 	addIngredientsToShoppingList(ingredients: Ingredient[]){
 		this.shoppingListService.addIngredients(ingredients);
 	}
+
+    addRecipe(recipe: Recipe){
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, recipe: Recipe){
+        this.recipes[index]=recipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number){
+        this.recipes.splice(index,1);
+        this.recipesChanged.next(this.recipes.slice());
+    }
 }
