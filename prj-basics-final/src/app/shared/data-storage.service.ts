1fadfe374348b3cec49d5427a70d7fcec8c01e46
cmorredd <chaitu.riims@gmail.com>
2018-12-27 11:23:20 -0500

    Fetch and Save Operations - Some changes for rxjs6

--- prj-basics-final/src/app/shared/data-storage.service.ts
+++ prj-basics-final/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from "rxjs/operators";
 
 @Injectable()
 export class DataStorageService{
@@ -13,9 +14,21 @@ export class DataStorageService{
 	}
 
 	getRecipes(){
-		 this.http.get('https://ng-recipe-book-bf550.firebaseio.com/recipes.json').subscribe(
-		 	(response: Response)=>{
+		 this.http.get('https://ng-recipe-book-bf550.firebaseio.com/recipes.json').pipe(map(
+		 	(response: Response) =>
+		 	{
+		 		console.log('Response='+response.json());
 		 		const recipes: Recipe[]= response.json();
+		 		for(let recipe of recipes){
+		 			if (!recipe['ingredients']){
+		 				recipe['ingredients']= [];
+		 			}
+		 		}
+		 		return recipes;
+		 	}
+		 )).subscribe(
+		 	(recipes: Recipe[])=>{
+		 		//const recipes: Recipe[]= response.json();
 		 		//console.log(recipes);
 		 		this.recipeService.setRecipes(recipes);
 		 	}
