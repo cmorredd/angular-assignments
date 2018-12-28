52b43c2ad7178071156e09c4b0b6aed5b471227f
cmorredd <chaitu.riims@gmail.com>
2018-12-28 18:09:59 -0500

    Created a Core Module and Moved Home and Header

--- prj-basics-final/src/app/app.module.ts
+++ prj-basics-final/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
@@ -13,21 +13,21 @@ import { AuthGuardService } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent   
-  ],
+    AppComponent
+   ],
   imports: [
     BrowserModule,        
     HttpModule,       
     SharedModule, 
     ShoppingListModule,
     AuthModule,
-    AppRoutingModule    
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [ShoppingListService,RecipeService,DataStorageService,AuthService,AuthGuardService],
   bootstrap: [AppComponent]
